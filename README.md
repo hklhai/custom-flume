23186aea6636bef8ae14815e87167191efd00cd2
hklhai <hkhai@outlook.com>
2019-04-19 11:16:01 +0800

    Flume自定义HttpSource

---
+++ README.md
@@ -0,0 +1,23 @@
+# Unicom IOT Flume
+自定义Http采集
+ 
+ 
+ 
+---
+> HK  
+> linh@bjhxqh.com
+
+## 注意项目务必不要使用springboot模板建立，否则flume无法找到自定义处理类
+
+
+### flume 远程Debug开启
+修改配置文件中flume-env.sh
+JAVA_OPTS="-Xmx20m -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y"
+
+
+### flume 启动
+bin/flume-ng agent --conf-file conf/http-kafka-conf.properties --name a1 -Dflume.root.logger=INFO,console
+bin/flume-ng agent --conf ./conf/ --conf-file conf/http-kafka-conf.properties --name a1 -Dflume.root.logger=INFO,console
+
+### IDEA配置
+增加remote，配置host、port即可，对remote进行debug（原理：JMX交互）
---
+++ src/main/java/com/hxqh/custom/HttpSourceJsonHandler.java
@@ -0,0 +1,83 @@
+package com.hxqh.custom;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.http.HTTPSourceHandler;
+import org.apache.flume.source.http.JSONHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Ocean lin on 2019/4/18.
+ *
+ * @author Ocean lin
+ */
+public class HttpSourceJsonHandler implements HTTPSourceHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JSONHandler.class);
+//    private final Type listType = new TypeToken<List<JSONEvent>>() {
+//    }.getType();
+
+    private final Type type = new TypeToken<Object>() {
+    }.getType();
+
+    private final Gson gson;
+
+    public HttpSourceJsonHandler() {
+        gson = new GsonBuilder().disableHtmlEscaping().create();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<Event> getEvents(HttpServletRequest request) throws Exception {
+        String charset = request.getCharacterEncoding();
+
+        //UTF-8 is default for JSON. If no charset is specified, UTF-8 is to
+        //be assumed.
+        if (charset == null) {
+            LOG.debug("Charset is null, default charset of UTF-8 will be used.");
+            charset = "UTF-8";
+        } else if (!(charset.equalsIgnoreCase("utf-8")
+                || charset.equalsIgnoreCase("utf-16")
+                || charset.equalsIgnoreCase("utf-32"))) {
+            LOG.debug("charset:" + charset);
+
+            LOG.error("Unsupported character set in request {}. "
+                    + "JSON handler supports UTF-8, "
+                    + "UTF-16 and UTF-32 only.", charset);
+            throw new UnsupportedCharsetException("JSON handler supports UTF-8, "
+                    + "UTF-16 and UTF-32 only.");
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream(), "UTF8"));
+        StringBuffer buff = new StringBuffer();
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            buff.append(line);
+        }
+        String replaceAll = buff.toString().replaceAll(" ", "");
+        replaceAll = replaceAll.replaceAll("\t", "");
+        
+        List<Event> newEvents = new ArrayList<Event>(10);
+        newEvents.add(EventBuilder.withBody(replaceAll.getBytes()));
+        return newEvents;
+    }
+
+    public void configure(Context context) {
+    }
+
+}
